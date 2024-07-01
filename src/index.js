c53402b69bc34be16ff709885b900bc707e7b75d
MaysamSolaymani <Maysam.Solaymani@gmail.com>
2024-07-01 12:05:23 +0330

    changed

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {func} from './mogule.js'
+import {myTimer} from './mogule.js'
 
 class Timer extends React.Component{
   render(){
-    return <h1> the time is {new Date().toLocaleTimeString()}</h1>
+    return <h2>the time is {new Date().toLocaleTimeString()}</h2>
   }
 }
 
--- src/mogule.js
+++ src/mogule.js
@@ -1,4 +1,5 @@
-export let func = () =>
-{
-    return 'the time is ';
-}
+export class myTimer {
+    render(){
+      return <h1> the time is {new Date().toLocaleTimeString()}</h1>
+    }
+  }
